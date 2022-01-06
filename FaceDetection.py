b7e48e07aa52284e62db0579476838c3d3b06835
Raj Aryan <18raj06@gmail.com>
2022-01-06 23:01:13 +0530

    face detector and face mesh

--- FaceDetection.py
+++ FaceDetection.py
@@ -2,32 +2,64 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
-pTime = 0
+	
 
-mpFaceDetection = mp.solutions.face_detection
-mpDraw = mp.solutions.drawing_utils
-faceDetection = mpFaceDetection.FaceDetection(0.75) #parameter helps in making it accurate
+	
 
-while True:
-	success, img = cap.read()
+class FaceDetector():
+	def __init__(self, minDetectionCon=0.5):
+		self.minDetectionCon = minDetectionCon
+		self.mpFaceDetection = mp.solutions.face_detection
+		self.mpDraw = mp.solutions.drawing_utils
+		self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon) #parameter helps in making it accurate
 
-	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	results = faceDetection.process(imgRGB)
-
-	#results.detections has id, bounding box dimensions, confidence score, coordinates of key points
-	if results.detections:
-		for id, detection in enumerate(results.detections):
+	def findFaces(self, img, draw=True):
+		self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		self.results = self.faceDetection.process(imgRGB)
+		#results.detections has id, bounding box dimensions, confidence score, coordinates of key points
+		bboxs = []
+		if self.results.detections:
+			for id, detection in enumerate(self.results.detections):
 			#mpDraw.draw_detection(img, detection) -> bounding box + key points are drawn
-			bbox_from_class = detection.location_data.relative_bounding_box
-			ih, iw, ic = img.shape
-			bbox = int(bbox_from_class.xmin*iw), int(bbox_from_class.ymin*ih), \ int(bbox_from_class.width*iw), int(bbox_from_class.height*ih)
-			cv2.rectangle(img, bbox, (255, 0, 255), 2)
+				bbox_from_class = detection.location_data.relative_bounding_box
+				ih, iw, ic = img.shape
+				bbox = int(bbox_from_class.xmin*iw), int(bbox_from_class.ymin*ih), \ int(bbox_from_class.width*iw), int(bbox_from_class.height*ih)
+				bboxs.append([id, bbox, detection.score])
 
-	cTime = time.time()
-	fps = 1/(cTime - pTime)
-	pTime = cTime
-	cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+				if draw:
+					img = self.drawBoundingBox(img, bbox)
+		return img, bboxs
 
-	cv2.imshow('Image', img)
-	cv2.waitKey(1)
+	def drawBoundingBox(self, img, bbox, l=30, thickness=5):
+		x, y, w, h = bbox
+		x1, y1 = x+w, y+h
+
+		cv2.line(img, (x, y), (x+l, y), (255, 0, 255), thickness)
+		cv2.line(img, (x, y), (x, y+l), (255, 0, 255), thickness)
+		cv2.line(img, (x1, y), (x1-l, y), (255, 0, 255), thickness)
+		cv2.line(img, (x1, y), (x1, y+l), (255, 0, 255), thickness)
+		cv2.line(img, (x, y1), (x+l, y1), (255, 0, 255), thickness)
+		cv2.line(img, (x, y1), (x, y1-l), (255, 0, 255), thickness)
+		cv2.line(img, (x1, y1), (x1-l, y1), (255, 0, 255), thickness)
+		cv2.line(img, (x1, y1), (x1, y-l), (255, 0, 255), thickness)
+		return img 
+
+
+def main():
+	cap = cv2.VideoCapture(0)
+	pTime = 0
+
+	while True:
+		success, img = cap.read()
+		#frame rate (fps)
+		cTime = time.time()
+		fps = 1/(cTime - pTime)
+		pTime = cTime
+		cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+
+		cv2.imshow('Image', img)
+		cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+	main()
---
+++ FaceMeshMin.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh()
+#specifications of drawings
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+
+while True:
+	success, img = cap.read()
+	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	results = faceMesh.process(imgRGB)
+	if results.multi_face_landmarks:
+		for id, faceLms in enumerate(results.multi_face_landmarks):
+			mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACE_CONNECTIONS, drawSpec, drawSpec)
+			for lm in faceLms.landmark:
+				ih, iw, ic = img.shape
+				x, y = int(lm.x*iw), int(lm.y*ih)
+
+	#frame rate
+	cTime = time.time()
+	fps = 1/(cTime-pTime)
+	cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+	pTime = cTime
+
+	cv2.imshow('Image', img)
+	cv2.waitKey(1)
